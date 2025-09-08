370a36a7dce25d69d32536d93b9f2390fc1eefea
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-09-08 21:58:57 +0500

    Task3

---
+++ Tyuiu.MolkovIS.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.MolkovIS.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.MolkovIS.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
