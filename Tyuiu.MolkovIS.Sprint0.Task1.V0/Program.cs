2c5951c8adb096b17209e780fc681cf936ad4a7d
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-09-08 21:14:58 +0500

    Complete Task1

--- Tyuiu.MolkovIS.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.MolkovIS.Sprint0.Task1.V0/Program.cs
@@ -3,5 +3,9 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        string name = "Иван";
+        int age = 17;
+        Console.WriteLine("Здравствуйте меня зовут" + name + "мне" + age + "лет");
+        Console.ReadKey();
     }
 }
