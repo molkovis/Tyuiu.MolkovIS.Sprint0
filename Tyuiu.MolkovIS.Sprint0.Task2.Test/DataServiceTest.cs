915386733c23312aae81983b96019c675b5038b1
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-09-08 21:32:47 +0500

    Added Task2

---
+++ Tyuiu.MolkovIS.Sprint0.Task2.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MolkovIS.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MolkovIS.Sprint0.Task2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessage()
+        {
+            string name = "Иван";
+            string res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Иван", res);
+
+        }
+    }
+}
---
+++ Tyuiu.MolkovIS.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿using Tyuiu.MolkovIS.Sprint0.Task2.V0.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Иван"));
+        Console.ReadKey();
+    }
+}
