e41e904a827325ef200694be66e7575b973df5b4
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-09-09 20:40:55 +0500

    Task7

--- Tyuiu.MolkovIS.Sprint0.Task7.V0/Program.cs
+++ Tyuiu.MolkovIS.Sprint0.Task7.V0/Program.cs
@@ -1,9 +1,57 @@
-﻿
+﻿using Tyuiu.MolkovIS.Sprint0.Task7.V0.Lib;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Title = "Спринт #0 | Выполнил: Мольков И. С.| ПКТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт#0                                                                *");
+        Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #0                                                              *");
+        Console.WriteLine("* Выполнил: Мольков Иван Сергеевич                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Написать консольную программу C#, которая суммирует значение двух       *");
+        Console.WriteLine("* одинаковых массивов по длине                                            *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+        Console.WriteLine("***************************************************************************");
+
+        int[] arrayNums1 = new int[] { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Значение элементов массива №1");
+        for (int i = 0; i < arrayNums1.Length; i++)
+        {
+            Console.WriteLine(arrayNums1[i]+", ");
+        }
+
+        int[] arrayNums2 = new int[] { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Значение элементов массива №2");
+        for (int i = 0; i < arrayNums2.Length; i++)
+        {
+            Console.WriteLine(arrayNums2[i] + ", ");
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        if (arrayNums1.Length == arrayNums2.Length)
+        {
+            int[] resultArray = DataService.AdditionArrays(arrayNums1, arrayNums2);
+
+            Console.WriteLine("Сумма элементов массива равна: ");
+            for (int i = 0; i < resultArray.Length;i++)
+            {
+                Console.WriteLine(resultArray[i] + ", ");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ошибка! Исходные массивы имеют разное количетсво элементов");
+        }
+        Console.ReadKey();
     }
 }
