b706068689717b1cd90c9baae8b8925d5204bcc5
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-09-09 19:29:26 +0500

    Task5

---
+++ Tyuiu.MolkovIS.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.MolkovIS.Sprint0.Task5.V0.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+        Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+        Console.WriteLine("A * B = " + DataService.Multiplcation(10, 10));
+        Console.WriteLine("A / B = " + DataService.Division(5, 0));
+        Console.ReadKey();
+
+    }
+}
