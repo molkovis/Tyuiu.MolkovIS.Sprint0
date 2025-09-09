0a2085774fc1a16b0ed64872d8999f4f7542ff30
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-09-09 19:06:59 +0500

    Task4

---
+++ Tyuiu.MolkovIS.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.MolkovIS.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.MolkovIS.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAddValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void CheckSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        [TestMethod]
+        public void CheckAMultiplcationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplcation(10, 5));
+        }
+        [TestMethod]
+        public void CheckDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
